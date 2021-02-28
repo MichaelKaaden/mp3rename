d6f21dd6d9c968069629fd7a00e3e6cd2a0675b7
Michael Kaaden <github@kaaden.net>
2021-02-28 22:52:37 +0100

    Get a vector of DirEntry

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
-use walkdir::{DirEntry, Error, WalkDir};
+use walkdir::{DirEntry, WalkDir};
 
 use crate::config::Config;
 
 pub mod config;
 
-pub fn get_list_of_dirs(config: &Config) -> Vec<Result<DirEntry, Error>> {
+/// Returns the list of directories.
+pub fn get_list_of_dirs(config: &Config) -> Vec<DirEntry> {
     WalkDir::new(&config.start_dir)
         .into_iter()
         .filter_entry(|e| e.file_type().is_dir())
+        // filter and report errors
         .filter(|e| match e {
             Ok(_) => true,
             Err(err) => {
@@ -15,5 +17,7 @@ pub fn get_list_of_dirs(config: &Config) -> Vec<Result<DirEntry, Error>> {
                 false
             }
         })
+        // convert to DirEntry
+        .map(|e| e.unwrap())
         .collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,14 @@ use mp3bandtitle::get_list_of_dirs;
 fn main() {
     let config = Config::new();
 
-    println!("The config is: \n{}", config);
-
-    println!("Path is {:?}", config.start_dir);
+    println!("==============");
+    println!("Configuration:");
+    println!("{}", config);
+    println!("==============");
 
     let entries = get_list_of_dirs(&config);
     println!("get_list_of_dirs() finished");
     for entry in entries {
-        match entry {
-            Ok(e) => println!("Got {}", e.path().display()),
-            Err(err) => eprintln!("Error iterating: {}", err),
-        }
+        println!("Got {}", entry.path().display());
     }
 }
