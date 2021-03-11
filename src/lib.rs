4f72f668d5c8e39038593fc814b0de5dbf8df651
Michael Kaaden <github@kaaden.net>
2021-03-11 19:35:26 +0100

    Move the tag extraction into the lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::fmt::Formatter;
 use std::{fmt, fs};
 
@@ -8,17 +9,18 @@ use crate::config::Config;
 
 pub mod config;
 
-pub struct DirContents<'a> {
+pub struct DirContents {
     pub dir_entry: walkdir::DirEntry,
     pub music_files: Vec<std::fs::DirEntry>, // contained music files
-    pub music_tags: Vec<Option<MusicTags<'a>>>, // tags read from the music_files
+    pub music_tags: Vec<Option<MusicTags>>,  // tags read from the music_files
     pub other_files: Vec<std::fs::DirEntry>, // contained other files (potentially being deleted)
 }
 
-impl<'a> DirContents<'a> {
+impl DirContents {
     pub fn new(config: &Config) -> Vec<DirContents> {
         let entries = get_list_of_dirs(&config);
-        get_dirs_with_music(entries)
+        let contents = get_dirs_with_music(entries);
+        contents
     }
 }
 
@@ -43,30 +45,41 @@ fn get_list_of_dirs(config: &Config) -> Vec<walkdir::DirEntry> {
 }
 
 /// Returns directories containing music files
-fn get_dirs_with_music<'a>(dirs: Vec<walkdir::DirEntry>) -> Vec<DirContents<'a>> {
+fn get_dirs_with_music<'a>(dirs: Vec<walkdir::DirEntry>) -> Vec<DirContents> {
     let mut dir_contents = vec![];
 
     for dir in dirs {
         if dir.file_type().is_dir() {
             let readdir = fs::read_dir(dir.path());
             if readdir.is_ok() {
-                let (music, others): (
-                    Vec<Result<std::fs::DirEntry, _>>,
-                    Vec<Result<std::fs::DirEntry, _>>,
-                ) = readdir
+                let (music, others): (Vec<std::fs::DirEntry>, Vec<std::fs::DirEntry>) = readdir
                     .unwrap()
                     .filter(|dir_entry| dir_entry.as_ref().unwrap().path().is_file())
-                    .partition(|dir_entry| {
-                        let entry = dir_entry.as_ref().unwrap();
-                        is_music_file(&entry)
-                    });
+                    .map(|dir_entry| dir_entry.unwrap())
+                    .partition(|dir_entry| is_music_file(dir_entry));
+
                 // only return directories containing music files
                 if music.len() > 0 {
+                    let music_tags: Vec<Option<MusicTags>> = music
+                        .iter()
+                        .map(|m| get_tags(m))
+                        //.map(|m| {
+                        //    Some(MusicTags {
+                        //        album: "".to_string(),
+                        //        artist: "".to_string(),
+                        //dir_entry: m, // diese Struktur und music haben beide diese Referenz, damit kann die Ownership nicht übertragen werden
+                        //disk_number: None,
+                        //title: "".to_string(),
+                        //track_number: 0,
+                        //})
+                        //})
+                        .collect();
+
                     dir_contents.push(DirContents {
                         dir_entry: dir,
-                        music_files: music.into_iter().map(|m| m.unwrap()).collect(),
-                        music_tags: vec![],
-                        other_files: others.into_iter().map(|o| o.unwrap()).collect(),
+                        music_files: music,
+                        music_tags,
+                        other_files: others,
                     });
                 }
             }
@@ -94,16 +107,16 @@ fn is_music_filename(file_name: &str) -> bool {
         || file_name.to_lowercase().ends_with(".m4v")
 }
 
-pub struct MusicTags<'a> {
+pub struct MusicTags {
     pub album: String,
     pub artist: String,
-    pub dir_entry: &'a std::fs::DirEntry,
+    //pub dir_entry: &'a std::fs::DirEntry,
     pub disk_number: Option<u16>,
     pub title: String,
     pub track_number: u16,
 }
 
-impl<'a> fmt::Display for MusicTags<'_> {
+impl fmt::Display for MusicTags {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         writeln!(f, "Album:        {}", self.album)?;
         if let Some(disk_number) = self.disk_number {
@@ -119,16 +132,15 @@ pub fn get_tags(music_file: &std::fs::DirEntry) -> Option<MusicTags> {
     let tag = audiotags::Tag::new()
         .read_from_path(music_file.path())
         .unwrap_or_else(|_| panic!("Could not read \"{}\"", music_file.path().to_string_lossy()));
-    let mut result: Option<MusicTags> = None;
 
     if let Some(album) = tag.album_title() {
         if let Some(artist) = tag.artist() {
             if let Some(title) = tag.title() {
                 if let Some(track_number) = tag.track_number() {
-                    result = Some(MusicTags {
+                    return Some(MusicTags {
                         album: album.to_string(),
                         artist: artist.to_string(),
-                        dir_entry: music_file,
+                        //dir_entry: music_file,
                         disk_number: tag.disc_number(),
                         title: title.to_string(),
                         track_number,
@@ -138,7 +150,30 @@ pub fn get_tags(music_file: &std::fs::DirEntry) -> Option<MusicTags> {
         }
     }
 
-    result
+    None
+}
+
+#[allow(dead_code)]
+fn sort_music_tag_func(a: &Option<MusicTags>, b: &Option<MusicTags>) -> Ordering {
+    let left = a.as_ref().unwrap_or_else(|| panic!("a is not defined"));
+    let right = b.as_ref().unwrap_or_else(|| panic!("b is not defined"));
+
+    if left.disk_number.is_some() && right.disk_number.is_none() {
+        return Ordering::Greater;
+    } else if left.disk_number.is_none() && right.disk_number.is_some() {
+        return Ordering::Less;
+    }
+
+    if let Some(left_disk_number) = left.disk_number {
+        if let Some(right_disk_number) = right.disk_number {
+            let disk_number_comparison = left_disk_number.cmp(&right_disk_number);
+            if disk_number_comparison != Ordering::Equal {
+                return disk_number_comparison;
+            }
+        }
+    }
+
+    left.track_number.cmp(&right.track_number)
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::cmp::Ordering;
 use std::process;
 
 use mp3bandtitle::config::Config;
-use mp3bandtitle::{get_tags, DirContents, MusicTags};
+use mp3bandtitle::DirContents;
 
 fn main() {
     let config = Config::new();
@@ -12,7 +11,7 @@ fn main() {
     println!("{}", config);
     println!("==============");
 
-    let mut contents = DirContents::new(&config);
+    let contents = DirContents::new(&config);
     for entry in &contents {
         println!("dir: {}", entry.dir_entry.path().to_str().unwrap());
         println!("music: {} entries", entry.music_files.len());
@@ -20,17 +19,6 @@ fn main() {
         for o in &entry.other_files {
             println!("other file: {}", o.path().to_string_lossy());
         }
-    }
-
-    for entry in &mut contents {
-        println!("=================");
-        let mut tags: Vec<Option<MusicTags>> = entry
-            .music_files
-            .iter()
-            .map(|music_file| get_tags(music_file))
-            .collect();
-        tags.sort_by(|a, b| sort_music_tag_func(a, b));
-        entry.music_tags = tags;
 
         for tag in &entry.music_tags {
             match tag {
@@ -45,26 +33,4 @@ fn main() {
             }
         }
     }
-}
-
-fn sort_music_tag_func(a: &Option<MusicTags>, b: &Option<MusicTags>) -> Ordering {
-    let left = a.as_ref().unwrap_or_else(|| panic!("a is not defined"));
-    let right = b.as_ref().unwrap_or_else(|| panic!("b is not defined"));
-
-    if left.disk_number.is_some() && right.disk_number.is_none() {
-        return Ordering::Greater;
-    } else if left.disk_number.is_none() && right.disk_number.is_some() {
-        return Ordering::Less;
-    }
-
-    if let Some(left_disk_number) = left.disk_number {
-        if let Some(right_disk_number) = right.disk_number {
-            let disk_number_comparison = left_disk_number.cmp(&right_disk_number);
-            if disk_number_comparison != Ordering::Equal {
-                return disk_number_comparison;
-            }
-        }
-    }
-
-    left.track_number.cmp(&right.track_number)
 }
