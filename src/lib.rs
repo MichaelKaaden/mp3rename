7395ba60f12cc014b136063bf8aa77b9d7d4b46f
Michael Kaaden <github@kaaden.net>
2021-03-15 21:41:15 +0100

    Refactor the code to rename the directory into a function of its own

--- src/lib.rs
+++ src/lib.rs
@@ -30,32 +30,7 @@ fn handle_directory(dir: &DirContents, config: &Config) {
     if let Some(album_title) = same_album_title {
         println!("Same album title: {}", album_title);
         if config.rename_directory {
-            let old_path = dir.dir_entry.path();
-            let old_dir_name = old_path
-                .file_name()
-                .unwrap_or_else(|| {
-                    panic!(
-                        "Cannot retrieve file name from {}",
-                        old_path.to_string_lossy()
-                    )
-                })
-                .to_string_lossy();
-            let new_path = old_path.with_file_name(OsString::from(album_title));
-            let new_dir_name = new_path
-                .file_name()
-                .unwrap_or_else(|| {
-                    panic!(
-                        "Cannot retrieve file name from {}",
-                        new_path.to_string_lossy()
-                    )
-                })
-                .to_string_lossy();
-            println!("Renaming \"{}\" to \"{}\"", old_dir_name, new_dir_name);
-            if !config.dry_run {
-                if let Err(e) = fs::rename(old_path, new_path) {
-                    eprintln!("Error renaming \"{}\": {}", old_dir_name, e);
-                }
-            }
+            rename_directory(dir, config, album_title)
         }
     } else {
         println!("Multiple album names.")
@@ -68,4 +43,37 @@ fn handle_directory(dir: &DirContents, config: &Config) {
         }
     }
     //println!("{}", dir);
+}
+
+/// Rename the directory according to the album title
+fn rename_directory(dir: &DirContents, config: &Config, album_title: &String) {
+    let old_path = dir.dir_entry.path();
+    let old_dir_name = old_path
+        .file_name()
+        .unwrap_or_else(|| {
+            panic!(
+                "Cannot retrieve file name from {}",
+                old_path.to_string_lossy()
+            )
+        })
+        .to_string_lossy();
+
+    let new_path = old_path.with_file_name(OsString::from(album_title));
+    let new_dir_name = new_path
+        .file_name()
+        .unwrap_or_else(|| {
+            panic!(
+                "Cannot retrieve file name from {}",
+                new_path.to_string_lossy()
+            )
+        })
+        .to_string_lossy();
+
+    println!("Renaming \"{}\" to \"{}\"", old_dir_name, new_dir_name);
+
+    if !config.dry_run {
+        if let Err(e) = fs::rename(old_path, new_path) {
+            eprintln!("Error renaming \"{}\": {}", old_dir_name, e);
+        }
+    }
 }
