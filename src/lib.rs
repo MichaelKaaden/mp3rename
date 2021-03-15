fed4e24b0d7195ed8eda4e569722188f0e825400
Michael Kaaden <github@kaaden.net>
2021-03-15 21:38:23 +0100

    Rename the containing directory according to the album title

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::ffi::OsString;
+use std::fs;
+
 use crate::config::Config;
 use crate::dir_contents::DirContents;
 
@@ -26,6 +29,34 @@ fn handle_directory(dir: &DirContents, config: &Config) {
     let same_album_title = dir.same_album_title();
     if let Some(album_title) = same_album_title {
         println!("Same album title: {}", album_title);
+        if config.rename_directory {
+            let old_path = dir.dir_entry.path();
+            let old_dir_name = old_path
+                .file_name()
+                .unwrap_or_else(|| {
+                    panic!(
+                        "Cannot retrieve file name from {}",
+                        old_path.to_string_lossy()
+                    )
+                })
+                .to_string_lossy();
+            let new_path = old_path.with_file_name(OsString::from(album_title));
+            let new_dir_name = new_path
+                .file_name()
+                .unwrap_or_else(|| {
+                    panic!(
+                        "Cannot retrieve file name from {}",
+                        new_path.to_string_lossy()
+                    )
+                })
+                .to_string_lossy();
+            println!("Renaming \"{}\" to \"{}\"", old_dir_name, new_dir_name);
+            if !config.dry_run {
+                if let Err(e) = fs::rename(old_path, new_path) {
+                    eprintln!("Error renaming \"{}\": {}", old_dir_name, e);
+                }
+            }
+        }
     } else {
         println!("Multiple album names.")
     }
